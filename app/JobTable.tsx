9f6312bd6fc4c7a1d5eab26734de981953ba5baa
Roy QIU <karoyqiu@gmail.com>
2023-07-16 20:10:43 +0800

    用列表代替表格。

--- app/JobTable.tsx
+++ app/JobTable.tsx
@@ -1,15 +1,64 @@
 import { useLiveQuery } from 'dexie-react-hooks';
+import db, { Job } from '../lib/db';
+
+type JobWithRecord = Job & {
+  recordTime?: Date,
+}
+
+const getJobs = async () => {
+  const jobs = await db.jobs.toArray();
+
+  return Promise.all(jobs.map(async (job): Promise<JobWithRecord> => {
+    const records = await db.records.where('jobId').equals(job.id!);
+    const record = await records.first();
+
+    return {
+      ...job,
+      recordTime: record?.time,
+    };
+  }));
+};
 
 export default function JobTable() {
+  const jobs = useLiveQuery(getJobs);
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <td>#</td>
-          <td>任务</td>
-          <td>提醒时间</td>
-        </tr>
-      </thead>
-    </table>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2 mx-4">
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}@{job.time}
+        </button>
+      ))}
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}
+        </button>
+      ))}
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}
+        </button>
+      ))}
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}
+        </button>
+      ))}
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}
+        </button>
+      ))}
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}
+        </button>
+      ))}
+      {jobs?.map((job) => (
+        <button key={job.id} className="btn">
+          {job.name}
+        </button>
+      ))}
+    </div>
   );
 }
